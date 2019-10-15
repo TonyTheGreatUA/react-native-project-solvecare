ce16b143f9360377d00c271616619bc89bdee862
TonyTheGreatUA <fpscinema1@gmail.com>
2019-10-15 10:45:29 +0300

    remade flat list

--- components/Component4.js
+++ components/Component4.js
@@ -1,51 +1,96 @@
 import React, {Component} from 'react';
-import {Text, View, FlatList, StyleSheet} from 'react-native';
-import {ListItem} from 'react-native-elements';
+import {Text, View, FlatList, Image, StyleSheet} from 'react-native';
+
 export class Component4 extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
-    this.getRemoteData();
+    this.state = {
+      data: [],
+      defaultLoadAmount: 5,
+    };
+  }
+  componentDidMount() {
+    this.makeRemoteRequest();
   }
 
-  getRemoteData = () => {
-    const url = 'https://randomuser.me/api/?seed=1&page=1&results=10';
-    fetch(url)
-      .then(res => res.json())
-      .then(res => {
-        this.setState({
-          data: res.results,
+  makeRemoteRequest = count => {
+    fetch(`https://randomuser.me/api/?results=100`)
+      .then(response => response.json())
+      .then(response => {
+        let data = [];
+        response.results.forEach((item, index) => {
+          data.push({
+            index: ++index,
+            firstName: item.name.first,
+            lastName: item.name.last,
+            avatar: item.picture.thumbnail,
+          });
         });
+        this.setState({data: data.slice()});
       })
-      .catch(error => {
-        console.log(error);
-      });
-  };
-
-  capFirstLetter = string => {
-    return string.charAt(0).toUpperCase() + string.slice(1);
-  };
-
-  renderNativeItem = item => {
-    const name =
-      this.capFirstLetter(item.name.first) +
-      ' ' +
-      this.capFirstLetter(item.name.last);
-    let photo = item.picture.thumbnail;
-    return <ListItem roundAvatar title={name} avatar={{uri: photo}} />;
+      .catch(err => console.log(err));
   };
 
   render() {
+    console.log(this.state.data);
     return (
-      <View>
+      <View style={styles.view}>
         <FlatList
           data={this.state.data}
-          renderItem={({item}) => this.renderNativeItem(item)}
+          initialNumToRender={this.state.defaultAmount}
+          renderItem={({item}) => (
+            <View style={styles.item}>
+              <View style={styles.itemRow}>
+                <Image style={styles.avatar} source={{uri: item.avatar}} />
+                <Text style={styles.index}>{item.index}</Text>
+                <Text style={styles.firstName}>{item.firstName}</Text>
+                <Text style={styles.lastName}>{item.lastName}</Text>
+              </View>
+            </View>
+          )}
+          keyExtractor={item => item.index}
         />
       </View>
     );
   }
 }
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  view: {
+    flex: 1,
+    textAlign: 'center',
+    paddingTop: 50,
+  },
+  item: {
+    flex: 1,
+    width: '100%',
+    height: 100,
+  },
+  itemRow: {
+    flexDirection: 'row',
+    margin: 5,
+  },
+  index: {
+    paddingLeft: 30,
+    fontSize: 22,
+    paddingRight: 15,
+    color: 'white',
+  },
+  firstName: {
+    fontSize: 22,
+    color: 'white',
+  },
+  lastName: {
+    paddingLeft: 3,
+    fontSize: 22,
+    color: 'white',
+  },
+  avatar: {
+    width: 100,
+    height: 100,
+    borderRadius: 50,
+    borderWidth: 2,
+    borderColor: 'white',
+  },
+});
 export default Component4;
