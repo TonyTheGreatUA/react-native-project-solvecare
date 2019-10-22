0a4fe1e1ddb808191cff8ec46fc3f918949dad9a
TonyTheGreatUA <fpscinema1@gmail.com>
2019-10-22 10:19:46 +0300

    combined reducers

--- src/store/reducers.js
+++ src/store/reducers.js
@@ -1,7 +1,9 @@
 /*eslint-disable*/
 import {combineReducers} from 'redux';
 import {formReducer} from './form/reducers';
+import {formDetails} from './details/reducers';
 
 export default combineReducers({
   form: formReducer,
+  details: formDetails,
 });
